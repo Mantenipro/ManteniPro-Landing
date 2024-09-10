702bd470ee33874cf5972307e186c5fd3c751ca9
Joseph Colin <joelbenjosephc@gmail.com>
2024-09-09 21:27:19 -0600

    Add HowWork component

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import { Montserrat, Source_Sans_3 } from 'next/font/google'
 import Nav from '@/components/Nav'
 import Hero from '@/components/Hero'
 import KeyBenefits from '@/components/KeyBenefits'
+import HowWork from '@/components/HowWork'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 const sourceSans3 = Source_Sans_3({ subsets: ['latin'] })
@@ -13,8 +14,8 @@ export default function Home() {
       <div className='h-dvh'>
         <Hero />
         <KeyBenefits />
+        <HowWork />
       </div>
-      
     </main>
   )
 }
