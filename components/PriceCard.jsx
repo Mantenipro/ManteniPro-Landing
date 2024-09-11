4fa312ef28de5a6c05e7ac6d5f524cd519b84207
AleCalz <calzadaloyaalejandra@gmail.com>
2024-09-11 14:47:13 -0600

    Fix style card button

--- components/PriceCard.jsx
+++ components/PriceCard.jsx
@@ -1,35 +1,53 @@
-const clsx = require('clsx');
+const clsx = require('clsx')
 
-export default function PriceCard({ title, price, description, btnTxt, icon, featureTitle, features }) {
+export default function PriceCard({
+  title,
+  price,
+  description,
+  btnTxt,
+  icon,
+  featureTitle,
+  features
+}) {
   return (
-    <div className={
-      clsx('p-5 max-w-screen-sm flex flex-col flex-1 border-2 border-[#3C5191] rounded-lg ', 
-          { 'text-white bg-[#3C5191]' : title === 'Avanzado' })}>
-       <h1 className='font-semibold mb-5 text-2xl'>{title}</h1>
-        <p className='text-lg mb-7'>{description}</p>
-        <span className='text-3xl font-semibold'>${price} MXN</span>
-          <button className={
-            clsx('text-[#3C5191] px-4 py-2 w-full border border-[#3C5191] mt-5 text-2xl', 
-                { ' bg-[#31416D] text-slate-50' : title === 'Avanzado' })}>
-            {btnTxt}
-          </button>
-          <p className='mt-7 font-semibold text-lg mb-5'>
-            {featureTitle}</p>
+    <div
+      className={clsx(
+        'flex max-w-screen-sm flex-1 flex-col rounded-lg border-2 border-[#3C5191] p-5 transition-transform duration-300 lg:hover:scale-105',
+        {
+          'bg-[#3C5191] text-white': title === 'Avanzado'
+        }
+      )}
+    >
+      <h1 className='mb-5 text-3xl font-semibold'>{title}</h1>
+      <p className='mb-7 text-lg'>{description}</p>
+      <span className='text-3xl font-semibold'>${price} MXN</span>
+      <button
+        className={clsx(
+          'mt-5 w-full rounded border border-[#3C5191] px-4 py-2 text-2xl text-[#3C5191] hover:font-medium hover:shadow-xl hover:shadow-[#bdc5db]',
+          {
+            'border-[#31416d] bg-[#31416D] text-slate-50 hover:shadow-[#302f2c]':
+              title === 'Avanzado'
+          }
+        )}
+      >
+        {btnTxt}
+      </button>
+      <p className='mb-5 mt-7 text-lg font-semibold'>{featureTitle}</p>
 
-          {/* iterar sobre features */}
-          <div>
-            <ul className='flex flex-col gap-4 my-2'>
-              {features.map((feature, index) => (
-                <li key={index} className='flex items-center '>
-                  <img src={`icon/${icon}`} alt="check-icon" />
-                  <div className='ml-2'>
-                    <strong >{feature.featureTitle || feature.title}</strong>
-                    {feature.desc && <p>{feature.desc}</p>}
-                    </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-      </div> 
+      {/* iterar sobre features */}
+      <div>
+        <ul className='my-2 flex flex-col gap-4'>
+          {features.map((feature, index) => (
+            <li key={index} className='flex items-center'>
+              <img src={`icon/${icon}`} alt='check-icon' />
+              <div className='ml-2'>
+                <strong>{feature.featureTitle || feature.title}</strong>
+                {feature.desc && <p>{feature.desc}</p>}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   )
 }
--- components/PriceSection.jsx
+++ components/PriceSection.jsx
@@ -1,88 +1,87 @@
 import PriceCard from '/components/PriceCard'
 
 export default function PriceSection() {
-
   const cardDetails = [
     {
       title: 'Básico',
       price: '100',
-      description:'Ideal para pequeñas empresas o proyectos que están comenzando.',
+      description:
+        'Ideal para pequeñas empresas o proyectos que están comenzando.',
       btnTxt: '¡Agenda tu Demo!',
       icon: 'check-icon-dark.png',
       featureTitle: 'Incluye Básico:',
-      features:[
+      features: [
         {
-          title: 'Gestión de Incidencias Limitada:', 
+          title: 'Gestión de Incidencias Limitada:',
           desc: 'Maneja hasta 10 incidencias al mes.'
         },
         {
-          title: 'Soporte por Email:', 
+          title: 'Soporte por Email:',
           desc: 'Respuesta en 48 a 72 horas.'
         },
         {
-          title: 'Historial de Incidencias:', 
+          title: 'Historial de Incidencias:',
           desc: 'Accede a las incidencias de los últimos 30 días.'
         },
         {
-          title: '1 Administrador:', 
+          title: '1 Administrador:',
           desc: 'Un usuario con permisos para gestionar el sistema.'
-        },
+        }
       ]
     },
-    
+
     {
       title: 'Avanzado',
       price: '500',
-      description:'Perfecto para equipos que necesitan más capacidad y características avanzadas.',
+      description:
+        'Perfecto para equipos que necesitan más capacidad y características avanzadas.',
       btnTxt: '¡Agenda tu Demo!',
       icon: 'check-icon-light.png',
       featureTitle: 'Todo lo Básico, además de:',
-      features:[
+      features: [
         {
-          title: 'Manejo Ampliado de Incidencias:', 
+          title: 'Manejo Ampliado de Incidencias:',
           desc: 'Gestiona hasta 100 incidencias al mes.'
         },
         {
-          title: 'Soporte Prioritario por Email:', 
+          title: 'Soporte Prioritario por Email:',
           desc: 'Respuesta en 24 a 48 horas.'
         },
         {
-          title: 'Historial de Incidencias Extendido: ', 
+          title: 'Historial de Incidencias Extendido: ',
           desc: 'Accede al historial de incidencias de hasta 6 meses.'
         },
         {
-          title: '2 Administradores:', 
+          title: '2 Administradores:',
           desc: 'Agrega un segundo administrador para compartir responsabilidades en la gestión.'
-        },
+        }
       ]
-    },
-  ];
+    }
+  ]
 
   return (
-    <section className="max-w-screen-2xl mx-auto py-16 px-4 ">
-        <h1 className="text-4xl font-medium mb-6 text-center">
-          ¡Conoce Mantenipro hoy mismo!
-        </h1>
-        <p className="text-gray-600 text-xl mb-12 max-w-screen-lg text-center m-auto">
-          Con Mantenipro, puedes empezar con una prueba gratuita de 30 días para explorar todas las funciones y decidir si se ajusta a tus necesidades.
-        </p>
+    <section className='mx-auto max-w-screen-2xl px-4 py-16'>
+      <h1 className='mb-6 text-center text-4xl font-medium'>
+        ¡Conoce Mantenipro hoy mismo!
+      </h1>
+      <p className='m-auto mb-12 max-w-screen-lg text-center text-xl text-gray-600'>
+        Con Mantenipro, puedes empezar con una prueba gratuita de 30 días para
+        explorar todas las funciones y decidir si se ajusta a tus necesidades.
+      </p>
       {/* Cards */}
-      <div className='flex flex-wrap gap-10 justify-center' >
-        {
-          cardDetails.map((card, index) => (
-          
-            <PriceCard
-              key={index}
-              title={card.title}
-              price={card.price}
-              description={card.description}
-              btnTxt={card.btnTxt}
-              icon={card.icon}
-              featureTitle={card.featureTitle}
-              features={card.features}
-            />
-          ))
-        }
+      <div className='flex flex-col flex-wrap items-center justify-center gap-16 lg:flex-row lg:items-stretch'>
+        {cardDetails.map((card, index) => (
+          <PriceCard
+            key={index}
+            title={card.title}
+            price={card.price}
+            description={card.description}
+            btnTxt={card.btnTxt}
+            icon={card.icon}
+            featureTitle={card.featureTitle}
+            features={card.features}
+          />
+        ))}
       </div>
     </section>
   )
