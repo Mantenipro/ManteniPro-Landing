8365ca0a817a4e385b1fcb1987ca4159a23fdd44
AleCalz <calzadaloyaalejandra@gmail.com>
2024-09-17 22:08:36 -0600

    Sorting class

--- components/AboutUs.jsx
+++ components/AboutUs.jsx
@@ -3,19 +3,19 @@ const sourceSans3 = Source_Sans_3({ subsets: ['latin'] })
 
 export default function AboutUs() {
   return (
-    <section className='my-14 lg:mt-36 w-full flex flex-col lg:flex-row items-center justify-center bg-[#e6eef8] gap-10  mx-auto p-4 md:px-10'>
-      <h2 className='text-slate-800 text-center text-3xl font-semibold mb-5 lg:hidden'>
+    <section className='mx-auto my-14 flex w-full flex-col items-center justify-center gap-10 bg-[#e6eef8] p-4 md:px-10 lg:mt-36 lg:flex-row'>
+      <h2 className='mb-5 text-center text-3xl font-semibold text-slate-800 lg:hidden'>
         Acerca de Mantenipro
       </h2>
-      <div className=' min-w-56 lg:-mt-28 '>
-        <img src='/img-aboutUs.png' alt='about us' className='w-full h-full' />
+      <div className='min-w-56 lg:-mt-28'>
+        <img src='/img-aboutUs.png' alt='about us' className='h-full w-full' />
       </div>
       <div className='max-w-screen-md'>
-        <h2 className='text-black text-center text-4xl font-medium mb-8 hidden lg:block'>
+        <h2 className='mb-8 hidden text-center text-4xl font-medium text-slate-800 lg:block'>
           Acerca de Mantenipro
         </h2>
         <p
-          className={`text-center text-lg md:mx-10 md:text-xl ${sourceSans3.className}`}
+          className={`text-center text-lg text-slate-700 md:mx-10 md:text-xl ${sourceSans3.className}`}
         >
           En Mantenipro, simplificamos el mantenimiento de equipos con
           tecnología eficiente, reduciendo costos y optimizando procesos. Nos
