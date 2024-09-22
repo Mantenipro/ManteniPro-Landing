1fb5e6a303b897fd3aec764a263c9d906340dac4
Ricardo Lopez <albelop1m@gmail.com>
2024-09-22 15:42:55 -0700

    chore: changes in the nav logo

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Nav() {
   return (
     <nav className='flex justify-between p-4 shadow-lg bg-white h-[80px]'>
-      <div className='flex justify-center items-center md:ml-[44px] h-[3rem] w-[10rem]'>
+      <div className='flex justify-center items-center md:ml-[44px] h-[3rem] w-[10rem] md:w-[14rem] lg:w-[16rem]'>
         <img className='' src='/logoManteniPro.svg' />
       </div>
       <div className='md:mr-[44px] flex justify-center items-center'>
